c520f09eab2e08f361c03ebe51a9b76923a0d61d
mdziezyc <mdziezyc@n7mobile.com>
2016-07-13 19:28:33 +0200

    finished _readwrapper

--- src/N2xInterface.py
+++ src/N2xInterface.py
@@ -1,4 +1,4 @@
-import socket
+from socket import socket, recv, sendall, SHUT_RDWR
 
 
 class N2xInterface(object):
@@ -19,6 +19,8 @@ class N2xInterface(object):
         self.frameMatchers = []
         self.stats = []
 
+        self._readWrapper.readBuffer = ""
+
         self.smAddress = address
         self.smPort = port
         self.smSocket = None
@@ -35,6 +37,10 @@ class N2xInterface(object):
 
     def connect(self):
         raise NotImplementedError()
+
+    def disconnect(self):
+
+    def shutdown(self):
         
     def invoke(self, interfaceName, methodName, args):
         data = "invoke " + interfaceName + " " + methodName + " " + args;
@@ -50,8 +56,24 @@ class N2xInterface(object):
     def _writeWrapper(self, socket, data):
         try:
             socket.sendall(data)
+
         except OSError:
             self.shutdown()
+            raise
 
-    def _readWrapper(self):
-        
+    def _readWrapper(self, socket):
+        try:
+            while "\r\n" not in self._readWrapper.readBuffer:
+                self._readWrapper.readBuffer += str(socket.recv(4096))
+
+            parts = self._readWrapper.readBuffer.partition("\r\n")
+            self._readWrapper.readBuffer = parts[2]
+            
+            result = int(parts[0].partition(' ')[0])
+            output = parts[0].partition(' ')[2]
+
+            return result, output
+
+        except OSError:
+            self.shutdown()
+            raise
